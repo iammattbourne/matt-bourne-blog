8c1e6d134492109882f2ac67365334b31932e5b2
Matt Bourne <mattbournemedia@gmail.com>
2023-04-06 15:46:19 +0100

    ADD: Astro Image Component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind(), image()]
 });
