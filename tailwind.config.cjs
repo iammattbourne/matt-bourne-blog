580e26ebd51412e39637c222c9fbd33951f483d0
Matt Bourne <mattbournemedia@gmail.com>
2023-05-22 18:40:41 +0100

    UPDATE: Markdown styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,13 +9,48 @@ module.exports = {
 		  current: 'currentColor',
 		  'brand-blue': '#1a56db',
 		  'brand-blue-light': '#8dabed',
+		  'heading-colour': '#111827',
+          'body-colour': '#6B7280'
 		},
 		extend:
 		{
+			typography: ({ theme }) => ({
+				mattBourne: {
+				  css: {
+					'--tw-prose-body': theme('colors.body-colour'),
+					'--tw-prose-headings': theme('colors.heading-colour'),
+					'--tw-prose-lead': theme('colors.heading-colour'),
+					'--tw-prose-links': theme('colors.brand-blue'),
+					'--tw-prose-bold': theme('colors.heading-colour'),
+					'--tw-prose-counters': theme('colors.body-colour'),
+					'--tw-prose-bullets': theme('colors.body-colour'),
+					'--tw-prose-hr': theme('colors.body-colour'),
+					'--tw-prose-quotes': theme('colors.heading-colour'),
+					'--tw-prose-quote-borders': theme('colors.brand-blue'),
+					'--tw-prose-captions': theme('colors.heading-colour'),
+					'--tw-prose-code': theme('colors.brand-blue'),
+					'--tw-prose-pre-code': theme('colors.brand-blue'),
+					'--tw-prose-pre-bg': theme('colors.brand-blue'),
+					'--tw-prose-th-borders': theme('colors.brand-blue-light'),
+					'--tw-prose-td-borders': theme('colors.brand-blue-light'),
+					p: {
+						'font-weight': '300',
+					},
+					'li::marker': {
+						'font-weight': '300',
+					},
+					li: {
+						'font-weight': '300',		
+					}
+				  },
+				},
+			  }),
 		  fontFamily: {
 			'heading': ['Inter', 'sans-serif'],
 			'body': ['Nunito', 'sans-serif'],
 		  },
+			
+	
 		},
 	},
 	plugins: [
